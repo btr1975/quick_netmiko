0c9b7c6b9ae0e37c9e98e728e7d504747694d56a
Ben <btr1975@gmail.com>
2020-09-08 14:55:07 -0500

    Added version info to the script

--- quick_netmiko/quick_netmiko.py
+++ quick_netmiko/quick_netmiko.py
@@ -1,6 +1,19 @@
+"""
+This is a simple class to get command from Cisco devices
+
+"""
 import ipaddress
 import socket
 from netmiko import ConnectHandler
+__author__ = 'Benjamin P. Trachtenberg'
+__copyright__ = "Copyright (c) 2020, Benjamin P. Trachtenberg"
+__credits__ = None
+__license__ = 'The MIT License (MIT)'
+__status__ = 'prod'
+__version_info__ = (1, 0, 0)
+__version__ = '.'.join(map(str, __version_info__))
+__maintainer__ = 'Benjamin P. Trachtenberg'
+__email__ = 'e_ben_75-python@yahoo.com'
 
 
 class FailedDnsLookup(Exception):
